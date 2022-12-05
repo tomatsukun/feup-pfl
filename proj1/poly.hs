2ca125c5cdea56261fe78ff1b60bb513f8d277e7
tomatsukun <tmsmcdpssl@gmail.com>
2022-12-05 15:48:45 +0000

    Up to date

--- proj1/poly.hs
+++ proj1/poly.hs
@@ -1,19 +1,16 @@
-type Indet = (Char, Int) -- Indeterminate, a variable and its exponent
-type Indets = [Indet] -- List of indeterminates on a monomial
-type Mono = (Int, Indets) -- Monomial as a coefficient and a set of indeterminates
-type Poly = [Mono] -- Polynomial as a list of monomials
+type Indet = (Char, Int)
+type Indets = [Indet]
+type Mono = (Int, Indets)
+type Poly = [Mono]
 
--- Collapses a list of strings into one, using sep between elements
 join :: String -> [String] -> String
 join sep xs = foldr (\a b-> a ++ if b=="" then b else sep ++ b) "" xs
 
--- Less than operator for the Indet type
 lessThanIndet :: Indet -> Indet -> Bool
 lessThanIndet (c1, expo1) (c2, expo2)
   | c1 == c2 = expo1 > expo2
   | otherwise = c1 < c2
 
--- Less than operator for the Indets type
 lessThanIndets :: Indets -> Indets -> Bool
 lessThanIndets inds [] = False
 lessThanIndets [] inds = True
@@ -22,7 +19,6 @@ lessThanIndets (ind1 : inds1) (ind2 : inds2)
   | lessThanIndet ind2 ind1 = False
   | otherwise = lessThanIndets inds1 inds2
 
--- Adds 2 polynomials, O(n), maintains normalization
 add :: Poly -> Poly -> Poly
 add [] [] = []
 add poly [] = poly
@@ -35,7 +31,6 @@ add ((coeff1, inds1) : poly1) ((coeff2, inds2) : poly2)
   | coeff1 + coeff2 == 0 = (add poly1 poly2)
   | otherwise = (coeff1 + coeff2, inds1) : (add poly1 poly2)
 
--- Multiplies 2 sets of Indets, O(n)
 multIndets :: Indets -> Indets -> Indets
 multIndets [] [] = []
 multIndets inds [] = inds
@@ -45,19 +40,16 @@ multIndets ((c1, expo1) : inds1) ((c2, expo2) : inds2)
   | c2 < c1 = (c2, expo2) : (multIndets ((c1, expo1) : inds1) inds2)
   | otherwise = (c1, expo1 + expo2) : (multIndets inds1 inds2)
 
--- Multiplies 2 monomials
 multMono :: Mono -> Mono -> Mono
 multMono (coeff1, inds1) (coeff2, inds2)
   | coeff1 * coeff2 == 0 = (0, [])
   | otherwise = (coeff1 * coeff2, multIndets inds1 inds2)
 
--- Multiplies 2 polynomials, maintains normalization
 multPoly :: Poly -> Poly -> Poly
 multPoly poly [] = []
 multPoly [] poly = []
 multPoly (mono1 : poly1) poly2 = add (map (multMono mono1) poly2) (multPoly poly1 poly2)
 
--- Derives monomial to given char, O(n)
 deriveMono :: Char -> Mono -> Mono
 deriveMono d (coeff, []) = (0, [])
 deriveMono d (0, inds) = (0, [])
@@ -67,7 +59,7 @@ deriveMono d (coeff, ((c, expo) : inds))
   | otherwise = (coeff * expo, ((c, expo - 1) : inds))
   where (newCoeff, newInds) = deriveMono d (coeff, inds)
 
--- Derives a polynomial to given char, O(n), maintains normalization
+
 derivePoly :: Char -> Poly -> Poly
 derivePoly c [] = []
 derivePoly c (mono : poly)
@@ -75,51 +67,36 @@ derivePoly c (mono : poly)
   | otherwise = (newCoeff, newInds) : (derivePoly c poly)
   where (newCoeff, newInds) = deriveMono c mono
 
--- Normalizes a given polynomial
-normalize :: Poly -> Poly
-normalize [] = []
-normalize ((coeff, inds) : poly)
-  | coeff == 0 = normalize poly
-  | otherwise = add [(coeff, inds)] (normalize poly)
-
--- Returns a string representation of a given indeterminate
-indetToString :: Indet -> String
-indetToString (c, expo)
+printIndet :: Indet -> String
+printIndet (c, expo)
   | expo == 1 = c : []
   | otherwise = c : '^' : (show expo)
 
--- Returns a string representation of a given indeterminate set
-indetsToString :: Indets -> String
-indetsToString inds = join "*" (map indetToString inds)
+printIndets :: Indets -> String
+printIndets inds = join "*" (map printIndet inds)
 
--- Returns a string representation of a given Poly head
-polyHeadToString :: Mono -> String
-polyHeadToString (coeff, inds)
-  | coeff == 1 = indetsToString inds
-  | coeff == -1 = '-' : (indetsToString inds)
-  | otherwise = (show coeff) ++ "*" ++ (indetsToString inds)
+printPolyHead :: Mono -> String
+printPolyHead (coeff, inds)
+  | coeff == 1 = printIndets inds
+  | coeff == -1 = '-' : (printIndets inds)
+  | otherwise = (show coeff) ++ "*" ++ (printIndets inds)
 
--- Returns a string representation of a given Poly body
-polyBodyToString :: Poly -> String
-polyBodyToString [] = ""
-polyBodyToString ((coeff, inds) : poly)
-  | coeff == 1 = " + " ++ (indetsToString inds) ++ (polyBodyToString poly)
-  | coeff == -1 = " - " ++ (indetsToString inds) ++ (polyBodyToString poly)
-  | coeff > 0 = " + " ++ (show coeff) ++ "*" ++ (indetsToString inds) ++ (polyBodyToString poly)
-  | coeff < 0 = " - " ++ (show (-coeff)) ++ "*" ++ (indetsToString inds) ++ (polyBodyToString poly)
+printPolyBody :: Poly -> String
+printPolyBody [] = ""
+printPolyBody ((coeff, inds) : poly)
+  | coeff == 1 = " + " ++ (printIndets inds) ++ (printPolyBody poly)
+  | coeff == -1 = " - " ++ (printIndets inds) ++ (printPolyBody poly)
+  | coeff > 0 = " + " ++ (show coeff) ++ "*" ++ (printIndets inds) ++ (printPolyBody poly)
+  | coeff < 0 = " - " ++ (show (-coeff)) ++ "*" ++ (printIndets inds) ++ (printPolyBody poly)
 
--- Returns a string representation of a given polynomial
-polyToString :: Poly -> String
-polyToString [] = "0"
-polyToString (mono : poly) = (polyHeadToString mono) ++ (polyBodyToString poly)
+printPoly :: Poly -> String
+printPoly [] = ""
+printPoly (mono : poly) = (printPolyHead mono) ++ (printPolyBody poly)
 
-main = do
-
-    print (polyToString (add [(2, [('a', 1)])] [(-2, [('a', 1)])]))
-    print (polyToString [(2, [('a', 3)]), (5, [('a', 1), ('b', 3)]), (3, [('x', 1)]), (-1, [('z', 5)])])
-    print (polyToString [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])])
-    print (polyToString (add [(2, [('a', 3)]), (5, [('a', 1), ('b', 3)]), (3, [('x', 1)]), (-1, [('z', 5)])] [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])]))
-    print (polyToString (multPoly [(2, [('a', 3)]), (3, [('b', 2)])] [(-5, [('a', 1)]), (-1, [('b', 5)])]))
-    print (polyToString [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])])
-    print (polyToString (derivePoly 'a' [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])]))
-    print (polyToString (normalize [(2, [('b', 1)]), (0, [('a', 2)]), (-5, [('a', 1)]), (1, [('c', 2)])]))
+main = do  
+    print (printPoly (add [(2, [('a', 1)])] [(3, [('a', 1), ('b', 3)])]))
+    print (printPoly [(2, [('a', 3)]), (5, [('a', 1), ('b', 3)]), (3, [('x', 1)]), (-1, [('z', 5)])])
+    print (printPoly [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])])
+    print (printPoly (add [(2, [('a', 3)]), (5, [('a', 1), ('b', 3)]), (3, [('x', 1)]), (-1, [('z', 5)])] [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])]))
+    print (printPoly (multPoly [(2, [('a', 3)]), (3, [('b', 2)])] [(-5, [('a', 1)]), (-1, [('b', 5)])]))
+    print (printPoly (derivePoly 'a' [(-1, [('a', 1)]), (-5, [('a', 3)]), (1, [('c', 2)])]))
---
+++ prolog-ap5/1.pl
@@ -0,0 +1,173 @@
+% :- use_module(library(lists)).
+:- dynamic male/1, female/1, parent/2.
+
+
+% format('~w ~w\n', [X,Y]), fail.
+clear :-
+    write('\033\[H\033\[2J').
+
+% 1. Relações familiares
+% a) database
+female(grace).
+female(haley).
+female(dede).
+female(gloria).
+female(barb).
+female(claire).
+female(pameron).
+female(lily).
+female(poppy).
+female(cameron).
+male(manny).
+male(frank).
+male(phil).
+male(jay).
+male(javier).
+male(merle).
+male(george).
+male(dylan).
+male(mitchell).
+male(joe).
+male(bo).
+male(calhoun).
+male(rexford).
+male(luke).
+male(alex).
+parent(grace, phil).
+parent(frank, phil).
+parent(dede, claire).
+parent(dede, mitchell).
+parent(jay, claire).
+parent(jay, mitchell).
+parent(jay, joe).
+parent(gloria, joe).
+parent(gloria, manny).
+parent(javier, manny).
+parent(barb, cameron).
+parent(barb, pameron).
+parent(merle, cameron).
+parent(merle, pameron).
+parent(phil, haley).
+parent(phil, alex).
+parent(phil, luke).
+parent(claire, haley).
+parent(claire, alex).
+parent(claire, luke).
+parent(mitchell, lily).
+parent(mitchell, rexford).
+parent(cameron, lily).
+parent(cameron, rexford).
+parent(pameron, calhoun).
+parent(bo, calhoun).
+parent(dylan, poppy).
+parent(dylan, george).
+parent(haley, poppy).
+parent(haley, george).
+
+% b) questions
+b1 :-
+    female(haley).
+b2 :-
+    male(gil).
+b3 :-
+    parent(frank, phil).
+b4(X) :-
+    parent(X, claire). % findall(Parent, parent(Parent, claire), X).
+b5(X) :-
+    parent(gloria, X).
+b6(X) :-
+    parent(jay, Child),
+    parent(Child, X).
+b7(X) :-
+    parent(X, Parent),
+    parent(Parent, lily).
+b8 :-
+    parent(alex, _).
+b9(X) :-
+    parent(jay, X),
+    \+ parent(gloria, X).
+
+% c) relations
+father(P, C) :-
+    male(P),
+    parent(P, C).
+
+grandparent(GP, GC) :-
+    parent(GP, P),
+    parent(P, GC).
+
+grandmother(GP, GC) :-
+    grandparent(GP, GC),
+    female(GP).
+
+siblings(S1, S2) :-
+    parent(P1, S1),
+    parent(P1, S2),
+    parent(P2, S1),
+    parent(P2, S2),
+    P1\=P2,
+    S1\=S2,
+    S1@<S2,
+    P1@<P2.
+
+halfSiblings(S1, S2) :-
+    parent(P1, S1),
+    parent(P1, S2),
+    parent(P2, S1),
+    parent(P3, S2),
+    P2\=P1,
+    P3\=P1,
+    S1\=S2,
+    P2\=P3.
+
+cousins(X, Y) :-
+    parent(P1, X),
+    parent(P2, Y),
+    siblings(P1, P2),
+    \+ siblings(X, Y).
+
+uncle(Uncle, Nephew) :-
+    male(Uncle),
+    siblings(Uncle, Parent),
+    parent(Parent, Nephew).
+
+% d) answers
+d1 :-
+    cousins(haley, lily).
+d2(Dad) :-
+    father(Dad, luke).
+d3(Uncle) :-
+    uncle(Uncle, lily).
+d4(Grandma) :-
+    grandmother(Grandma, _).
+
+
+
+
+% 1. Family
+% a)
+
+add_person:-
+   write('Person to add Gender-Person'),nl,
+   read(Gender-Person),
+   \+male(Person), \+female(Person),
+   add_person_handler(Gender, Person).
+add_person:- write('Person already exists'), fail.
+
+add_person_handler(Gender,Person):- Gender = male, assert(male(Person)).
+add_person_handler(Gender,Person):- Gender = female, assert(female(Person)).
+add_person_handler(_,_):- write('Gender should be male or female'), fail.
+
+
+% b)
+
+add_parents(Person):-
+   write('Add parents'),nl,
+   read(Father-Mother),
+   \+ parent(Father, Person), \+ parent(Mother, Person),
+   assert(parent(Father, Person)), assert(parent(Father, Person)).
+add_parents(_):- write('Parents already exist for given person'), fail.
+
+
+
+
